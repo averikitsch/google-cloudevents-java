653e8a2968c9504fbf4c3f1d7248dca24558852d
averikitsch <akitsch@google.com>
2022-10-10 12:36:52 -0700

    test protoc custom Plugin

--- protoc-gen-java-snowpea/src/main/java/com/google/MyProtocPlugin.java
+++ protoc-gen-java-snowpea/src/main/java/com/google/MyProtocPlugin.java
@@ -1,99 +1,152 @@
-import com.google.protobuf.DescriptorProtos;
-import com.google.protobuf.Descriptors;
-import com.google.protobuf.compiler.PluginProtos.CodeGeneratorResponse;
-import com.google.protobuf.compiler.PluginProtos.CodeGeneratorRequest;
 
+
+// package com.google.events;
+
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Collections;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
+import org.apache.velocity.app.VelocityEngine;
+import com.google.protobuf.Descriptors;
+import com.google.protobuf.DescriptorProtos.DescriptorProto;
+import com.google.protobuf.DescriptorProtos.EnumDescriptorProto;
+import com.google.protobuf.DescriptorProtos.FieldDescriptorProto;
+import com.google.protobuf.DescriptorProtos.FileDescriptorProto;
+import com.google.protobuf.Descriptors.FieldDescriptor;
+import com.google.protobuf.compiler.PluginProtos.CodeGeneratorRequest;
+import com.google.protobuf.compiler.PluginProtos.CodeGeneratorResponse;
 
 public class MyProtocPlugin {
 
     public static void main(String[] args) throws IOException, Descriptors.DescriptorValidationException {
         // Plugin receives a serialized CodeGeneratorRequest via stdin
         CodeGeneratorRequest request = CodeGeneratorRequest.parseFrom(System.in);
+        CodeGeneratorResponse.Builder response = CodeGeneratorResponse.newBuilder();
+        // CodeGeneratorRequest contain FileDescriptorProtos for all the proto files and dependencies
+        // Convert the FileDescriptorProtos into FileDescriptor instances,
+        // since they are easier to work with. 
 
-        // CodeGeneratorRequest contain FileDescriptorProtos for all the proto files we need to process
-        // as well as their dependencies.  We want to convert the FileDescriptorProtos into FileDescriptor instances,
-        // since they are easier to work with. We will build a map that maps file names to the corresponding file
-        // descriptor.
+        // FileDescriptor Describes a .proto file, including everything defined within. 
+        // descriptors for all the messages and file descriptors for all other imported .proto files (dependencies).
         Map<String, Descriptors.FileDescriptor> filesByName = new HashMap<>();
 
-        for (DescriptorProtos.FileDescriptorProto fp: request.getProtoFileList()) {
+        for (FileDescriptorProto protoFile : request.getProtoFileList()) {
             // The dependencies of fp are provided as strings, we look them up in the map as we are generating it.
             Descriptors.FileDescriptor dependencies[] =
-                    fp.getDependencyList().stream().map(filesByName::get).toArray(Descriptors.FileDescriptor[]::new);
-
-            Descriptors.FileDescriptor fd  = Descriptors.FileDescriptor.buildFrom(fp, dependencies);
-            filesByName.put(
-                    fp.getName(),
-                    fd
-            );
+                    protoFile.getDependencyList().stream().map(filesByName::get).toArray(Descriptors.FileDescriptor[]::new);
+            // System.out.println(protoFile.getName()); // file name
+            // System.out.println("");
+            // System.out.println(protoFile.getPackage()); // package
+            // System.out.println("deps");
+            // System.out.println(protoFile.getDependencyList()); // list of dep proto files
+            // System.out.println("enum");
+            // System.out.println(protoFile.getEnumTypeList());
+            // System.out.println("msgs");
+            // System.out.println(protoFile.getMessageTypeList());
+            // System.out.println("");
+            
+            System.out.println(protoFile.getDescriptor());
+            for (Descriptors.FileDescriptor value : dependencies) {
+                System.out.println(value.toString());
+            }
+            // Descriptors.FileDescriptor fd  = Descriptors.FileDescriptor.buildFrom(protoFile, dependencies);
+            // System.out.println(fd);
+            // filesByName.put(
+            //         fp.getName(),
+            //         fd
+            // );
         }
 
         // Building the response
-        CodeGeneratorResponse.Builder response = CodeGeneratorResponse.newBuilder();
-
-        for (String fileName : request.getFileToGenerateList()) {
-            Descriptors.FileDescriptor fd = filesByName.get(fileName);
-            response.addFileBuilder()
-                    .setName(fd.getFullName().replaceAll("\\.proto$", ".txt"))
-                    .setContent(generateFileContent(fd));
-        }
 
         // Serialize the response to stdout
-        response.build().writeTo(System.out);
+        // response.build().writeTo(System.out);
     }
-
-    private static String generateFileContent(Descriptors.FileDescriptor fd) {
+    private static String generateFileContent(DescriptorProto message, String packageName) throws IOException {
         StringBuilder sb = new StringBuilder();
-        for (Descriptors.Descriptor messageType : fd.getMessageTypes()) {
-            generateMessage(sb, messageType, 0);
-        }
+        generateMessage(sb, message);
+
         return sb.toString();
     }
+    // private static String generateFileContent(Descriptors.FileDescriptor fd, String packageName) throws IOException {
+    //     StringBuilder sb = new StringBuilder();
+    //     for (Descriptors.Descriptor messageType : fd.getMessageTypes()) {
+    //         generateMessage(sb, messageType, packageName);
+    //     }
+    //     return sb.toString();
+    // }
+    // for (DescriptorProto message : protoFile.getMessageTypeList()) {
+    //     String content = generateFileContent(message, protoFile.getPackage());
+    //     String fullPath = protoFile.getName().replaceAll("data",  message.getName()).replaceAll("\\.proto$", ".java");
+    //     if (saved.get(fullPath) == null) {
+    //         saved.put(fullPath, true);
+    //         response.addFileBuilder()
+    //             .setName(fullPath)
+    //             .setContent("package " + protoFile.getPackage() + ";\n" + content);
+    //     }
+    //     // messagesByName.put(
+    //     //     message.getName(),
+    //     //     fd
+    //     // );
+    // }
 
-    private static String renderType(Descriptors.FieldDescriptor fd) {
-        if (fd.isRepeated()) {
+    private static String renderType(FieldDescriptorProto fd) {
+        if (fd.getLabel().equals(FieldDescriptorProto.Label.LABEL_REQUIRED_VALUE)) {
             return "List<" + renderSingleType(fd) + ">";
         } else {
             return renderSingleType(fd);
         }
     }
 
-    private static String renderSingleType(Descriptors.FieldDescriptor fd) {
-        if (fd.getType() != Descriptors.FieldDescriptor.Type.MESSAGE) {
-            return fd.getType().toString();
+    private static String renderSingleType(FieldDescriptorProto fd) {
+        if (fd.getType() != FieldDescriptorProto.Type.TYPE_MESSAGE) {
+            return Descriptors.FieldDescriptor.Type.valueOf(fd.getType()).getJavaType().toString();
         } else {
-            return fd.getMessageType().getName();
+            return fd.getTypeName();
         }
     }
 
-    private static void generateMessage(StringBuilder sb, Descriptors.Descriptor messageType, int indent) {
-        sb.append(String.join("", Collections.nCopies(indent, " ")));
-        sb.append("|- ");
-        sb.append(messageType.getName());
-        sb.append("(");
+    private static void generateMessage(StringBuilder sb, DescriptorProto message) throws IOException {
+    // private static void generateMessage(StringBuilder sb, Descriptors.Descriptor messageType, String packageName) throws IOException {
+ 
+        String className = message.getName();
 
-        sb.append(
-            String.join(
-                ", ", 
-                messageType
-                    .getFields()
-                    .stream()
-                    .map(field -> field.getName() + ": " + renderType(field))
-                    .collect(Collectors.joining(", "))
-            )
-        );
-        sb.append(")");
-        sb.append(System.getProperty("line.separator"));
-
+        List<String> fields = message.getFieldList()
+                                .stream()
+                                .map(field -> renderType(field) + " " + field.getName())
+                                .collect(Collectors.toList());
+      
+        sb.append(getTemplate(className, fields));
         // recurse for nested messages.
-        sb.append(String.join("", Collections.nCopies(indent, " ")));
-        for (Descriptors.Descriptor nestedType : messageType.getNestedTypes()) {
-            generateMessage(sb, nestedType, indent + 3);
+        // sb.append(String.join("", Collections.nCopies(indent, " ")));
+        for (DescriptorProto nestedType : message.getNestedTypeList()) {
+            generateMessage(sb, nestedType);
         }
+        // sb.append(getTemplate(packageName, className, fields));
+        sb.append("}");
     }
+
+    public static String getTemplate( String className, List<String> fields) throws IOException {
+        String inputTemplate = "./src/main/resources/classTemplate.vm";
+        VelocityEngine velocityEngine = new VelocityEngine();
+        velocityEngine.init();
+
+        VelocityContext context = new VelocityContext();
+        // context.put("packageName", packageName);
+        context.put("className", className);
+        context.put("fields", fields);
+
+        StringWriter writer = new StringWriter();
+        Velocity.mergeTemplate(inputTemplate, "UTF-8", context, writer);
+        return writer.toString();
+    }
+    
 }
--- protoc-gen-java-snowpea/src/main/java/com/google/events/Plugin.java
+++ protoc-gen-java-snowpea/src/main/java/com/google/events/Plugin.java
@@ -1,207 +1,102 @@
 package com.google.events;
+import com.google.protobuf.DescriptorProtos;
+import com.google.protobuf.Descriptors;
+import com.google.protobuf.Descriptors.Descriptor;
+import com.google.protobuf.compiler.PluginProtos.CodeGeneratorResponse;
+import com.google.protobuf.compiler.PluginProtos.CodeGeneratorRequest;
 
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.StringWriter;
-import java.io.Writer;
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import org.apache.velocity.Template;
-import org.apache.velocity.VelocityContext;
-import org.apache.velocity.app.Velocity;
-import org.apache.velocity.app.VelocityEngine;
-import com.google.protobuf.Descriptors;
-import com.google.protobuf.DescriptorProtos.DescriptorProto;
-import com.google.protobuf.DescriptorProtos.EnumDescriptorProto;
-import com.google.protobuf.DescriptorProtos.FieldDescriptorProto;
-import com.google.protobuf.DescriptorProtos.FileDescriptorProto;
-import com.google.protobuf.Descriptors.FieldDescriptor;
-import com.google.protobuf.compiler.PluginProtos.CodeGeneratorRequest;
-import com.google.protobuf.compiler.PluginProtos.CodeGeneratorResponse;
 
 public class Plugin {
 
     public static void main(String[] args) throws IOException, Descriptors.DescriptorValidationException {
         // Plugin receives a serialized CodeGeneratorRequest via stdin
         CodeGeneratorRequest request = CodeGeneratorRequest.parseFrom(System.in);
-        CodeGeneratorResponse.Builder response = CodeGeneratorResponse.newBuilder();
+
         // CodeGeneratorRequest contain FileDescriptorProtos for all the proto files we need to process
         // as well as their dependencies.  We want to convert the FileDescriptorProtos into FileDescriptor instances,
         // since they are easier to work with. We will build a map that maps file names to the corresponding file
         // descriptor.
         Map<String, Descriptors.FileDescriptor> filesByName = new HashMap<>();
 
-        Map<String, Boolean> saved = new HashMap<>();
+        for (DescriptorProtos.FileDescriptorProto fp: request.getProtoFileList()) {
+            // The dependencies of fp are provided as strings, we look them up in the map as we are generating it.
+            Descriptors.FileDescriptor dependencies[] =
+                    fp.getDependencyList().stream().map(filesByName::get).toArray(Descriptors.FileDescriptor[]::new);
 
-        for (FileDescriptorProto protoFile : request.getProtoFileList()) {
-            for (DescriptorProto message : protoFile.getMessageTypeList()) {
-                String content = generateFileContent(message, protoFile.getPackage());
-                String fullPath = protoFile.getName().replaceAll("data",  message.getName()).replaceAll("\\.proto$", ".java");
-                if (saved.get(fullPath) == null) {
-                    saved.put(fullPath, true);
-                    response.addFileBuilder()
-                        .setName(fullPath)
-                        .setContent("package " + protoFile.getPackage() + ";\n" + content);
-                }
-                // messagesByName.put(
-                //     message.getName(),
-                //     fd
-                // );
-            }
+            Descriptors.FileDescriptor fd  = Descriptors.FileDescriptor.buildFrom(fp, dependencies);
+
+            filesByName.put(
+                    fp.getName(),
+                    fd
+            );
         }
 
         // Building the response
-        // CodeGeneratorResponse.Builder response = CodeGeneratorResponse.newBuilder();
+        CodeGeneratorResponse.Builder response = CodeGeneratorResponse.newBuilder();
 
-        // for (String fileName : request.getFileToGenerateList()) {
-        //     Descriptors.FileDescriptor fd = filesByName.get(fileName);
-        //     // for (DescriptorProto message : messages) {
-        //         //                 String fullPath = proto.getName().replace("data", message.getName())
-        //         //                         .replaceAll("\\.proto$", ".java");
-        //     String content = generateFileContent(fd, fd.getPackage());
-        //     // System.out.println(content);
-        //     System.out.println("mee" +fd.getFullName().replaceAll("\\.proto$", ".java"));
-        //     response.addFileBuilder()
-        //             .setName(fd.getFullName().replaceAll("\\.proto$", ".java"))
-        //             .setContent(content);
-        // }
+        for (String fileName : request.getFileToGenerateList()) {
+            Descriptors.FileDescriptor fd = filesByName.get(fileName);
+            response.addFileBuilder()
+                    .setName(fd.getFullName().replaceAll("\\.proto$", ".txt"))
+                    .setContent(generateFileContent(fd));
+        }
 
         // Serialize the response to stdout
         response.build().writeTo(System.out);
     }
-    private static String generateFileContent(DescriptorProto message, String packageName) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        generateMessage(sb, message);
 
+    private static String generateFileContent(Descriptors.FileDescriptor fd) {
+        StringBuilder sb = new StringBuilder();
+        for (Descriptors.Descriptor messageType : fd.getMessageTypes()) {
+            generateMessage(sb, messageType, 0);
+        }
         return sb.toString();
     }
-    // private static String generateFileContent(Descriptors.FileDescriptor fd, String packageName) throws IOException {
-    //     StringBuilder sb = new StringBuilder();
-    //     for (Descriptors.Descriptor messageType : fd.getMessageTypes()) {
-    //         generateMessage(sb, messageType, packageName);
-    //     }
-    //     return sb.toString();
-    // }
 
-    private static String renderType(FieldDescriptorProto fd) {
-        if (fd.getLabel().equals(FieldDescriptorProto.Label.LABEL_REQUIRED_VALUE)) {
+    private static String renderType(Descriptors.FieldDescriptor fd) {
+        if (fd.isRepeated()) {
             return "List<" + renderSingleType(fd) + ">";
         } else {
             return renderSingleType(fd);
         }
     }
 
-    private static String renderSingleType(FieldDescriptorProto fd) {
-        if (fd.getType() != FieldDescriptorProto.Type.TYPE_MESSAGE) {
-            return Descriptors.FieldDescriptor.Type.valueOf(fd.getType()).getJavaType().toString();
+    private static String renderSingleType(Descriptors.FieldDescriptor fd) {
+        if (fd.getType() != Descriptors.FieldDescriptor.Type.MESSAGE) {
+            return fd.getType().toString();
         } else {
-            return fd.getTypeName();
+            return fd.getMessageType().getName();
         }
     }
 
-    private static void generateMessage(StringBuilder sb, DescriptorProto message) throws IOException {
-    // private static void generateMessage(StringBuilder sb, Descriptors.Descriptor messageType, String packageName) throws IOException {
- 
-        String className = message.getName();
+    private static void generateMessage(StringBuilder sb, Descriptors.Descriptor messageType, int indent) {
+        sb.append(String.join("", Collections.nCopies(indent, " ")));
+        sb.append("|- ");
+        sb.append(messageType.getName());
+        sb.append("(");
 
-        List<String> fields = message.getFieldList()
-                                .stream()
-                                .map(field -> renderType(field) + " " + field.getName())
-                                .collect(Collectors.toList());
-      
-        sb.append(getTemplate(className, fields));
+        sb.append(
+            String.join(
+                ", ", 
+                messageType
+                    .getFields()
+                    .stream()
+                    .map(field -> field.getName() + ": " + renderType(field))
+                    .collect(Collectors.joining(", "))
+            )
+        );
+        sb.append(")");
+        sb.append(System.getProperty("line.separator"));
+
         // recurse for nested messages.
-        // sb.append(String.join("", Collections.nCopies(indent, " ")));
-        for (DescriptorProto nestedType : message.getNestedTypeList()) {
-            generateMessage(sb, nestedType);
+        sb.append(String.join("", Collections.nCopies(indent, " ")));
+        for (Descriptors.Descriptor nestedType : messageType.getNestedTypes()) {
+            generateMessage(sb, nestedType, indent + 3);
         }
-        // sb.append(getTemplate(packageName, className, fields));
-        sb.append("}");
     }
-
-    public static String getTemplate( String className, List<String> fields) throws IOException {
-        String inputTemplate = "./src/main/resources/classTemplate.vm";
-        VelocityEngine velocityEngine = new VelocityEngine();
-        velocityEngine.init();
-
-        VelocityContext context = new VelocityContext();
-        // context.put("packageName", packageName);
-        context.put("className", className);
-        context.put("fields", fields);
-
-        StringWriter writer = new StringWriter();
-        Velocity.mergeTemplate(inputTemplate, "UTF-8", context, writer);
-        return writer.toString();
-    }
-    
 }
-
-//     public static void main(String[] args) throws IOException {
-//         CodeGeneratorRequest request = CodeGeneratorRequest.parseFrom(System.in);
-//         List<FileDescriptorProto> protos = request.getProtoFileList()
-//         Map<String, Boolean> saved = new HashMap<>();
-
-//         CodeGeneratorResponse.Builder response = CodeGeneratorResponse.newBuilder();
-//         for (FileDescriptorProto proto : protos) {
-//             String packageName = proto.getPackage();
-//             List<EnumDescriptorProto> enums = proto.getEnumTypeList();
-//             // files.put(proto.getName(), proto.getMessageTypeList());
-//             List<DescriptorProto> messages = proto.getMessageTypeList();
-//             for (DescriptorProto message : messages) {
-//                 String fullPath = proto.getName().replace("data", message.getName())
-//                         .replaceAll("\\.proto$", ".java");
-//                 String tmp = generateClass(message, packageName);
-//                 // System.out.println(tmp);
-//                 if (saved.get(fullPath) == null) {
-//                     saved.put(fullPath, true);
-//                     response.addFileBuilder() // uppercasae name and fix regex
-//                             .setName(fullPath).setContent(tmp); //
-//                 }
-//             }
-//             // for (EnumDescriptorProto enumProto : enums) {
-//             //     String fullPath = proto.getName().replace("data", enumProto.getName())
-//             //             .replaceAll("\\.proto$", ".java");
-//             //     if (saved.get(fullPath) == null) {
-//             //         response.addFileBuilder() // uppercasae name and fix regex
-//             //                 .setName(fullPath).setContent(generateEnum(enumProto, packageName));
-//             //     }
-//             // }
-//             // String name = message.getName();
-
-//             // Map<FieldDescriptor, Object> fields = message.getAllFields();
-//             // messages.put(name, message);
-
-//             // }
-//             // filesByName.put(
-//             // proto.getName(),
-//             // fd
-//             // );
-
-//         }
-
-
-//         response.build().writeTo(System.out);
-//     }
-
-//     public static String generateClass(DescriptorProto message, String packageName) throws IOException {
-//         return getTemplate(packageName, message.getName(), message);
-//         // "package " + packageName + ";\n" + "\n" + "public class " + message.getName()
-//         //         + " {\n" + "\n" + message.getAllFields().toString();
-//     }
-
-//     public static String generateEnum(EnumDescriptorProto enumP, String packageName) {
-//         return "package " + packageName + ";\n" + "\n" + "public enum " + enumP.getName() + " {\n"
-//                 + "\n" + enumP.getAllFields().toString();
-//     }
-
-//     public static String getFields(DescriptorProto message) {
-//         Map<FieldDescriptor, Object> fields = message.getAllFields();
-//         // for (fields.get(fields))
-//         return "";
-//     }
-
-    
-// }
